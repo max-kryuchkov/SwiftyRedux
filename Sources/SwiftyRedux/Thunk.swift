270a713cf65c0d37a8650fcf7002eb88858f3a34
max kruchkov <max.kryuchkov111@gmail.com>
2022-07-25 15:57:13 +0300

    Thunk body marked as public

--- Sources/SwiftyRedux/Thunk.swift
+++ Sources/SwiftyRedux/Thunk.swift
@@ -9,9 +9,9 @@ import Foundation
 
 open class Thunk<S: AppState>: Action {
     
-    let body: (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void
+    public let body: (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void
     
-    init(body: @escaping (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void) {
+    public init(body: @escaping (_ dispatch: @escaping DispatchFunction, _ getState: @escaping () -> S?) -> Void) {
         self.body = body
     }
 }
